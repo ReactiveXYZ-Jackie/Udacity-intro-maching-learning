1318c844e6b46ee9befbe64d7529d7450dbf76d9
Reactive XYZ Dev <jackie@reactive.xyz>
2016-12-09 21:37:35 -0500

    Update
    - finished validation class (1st stage)

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,10 +12,12 @@
 
 import pickle
 import sys
-sys.path.append("../tools/")
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'tools'))
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+fp = os.path.join(os.path.dirname(__file__), '../','final_project', 'final_project_dataset.pkl')
+data_dict = pickle.load(open(fp, "r") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -28,5 +30,11 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
-
-
+# split training and testing data
+from sklearn import model_selection
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(features, labels ,test_size = .3, random_state = 42)
+# create classifier
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
